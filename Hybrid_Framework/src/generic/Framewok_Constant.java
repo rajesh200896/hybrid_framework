787910fb2aa6537b208f077c200c353937de42ba
HP <HP@DESKTOP-GKK5KKN>
2023-05-03 12:03:11 +0530

    framework_constant

---
+++ Hybrid_Framework/src/generic/Framewok_Constant.java
@@ -0,0 +1,12 @@
+package generic;
+
+public interface Framewok_Constant {
+	String gecko_key="webdriver.gecko.driver";
+	String gecko_value="./software/geckodriver.exe";
+		 
+	String chrome_key="webdriver.chrome.driver";
+	String chrome_value="./software/chromedriver.exe";
+
+	
+	String URL="https://www.facebook.com";
+}
